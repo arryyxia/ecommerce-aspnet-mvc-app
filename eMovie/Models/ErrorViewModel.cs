45dd868c780d35b051c9ac34353de39bd84aec1f
user <user@MYPCONEPROK7>
2023-08-21 14:37:29 +0800

    Add project files.

---
+++ eMovie/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace eMovie.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
